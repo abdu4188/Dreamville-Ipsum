9c3298191c0f3019c40365b5904f9394682cd1cc
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2020-08-28 13:03:56 +0300

    ⚡️ 💄 image  size reduction and seo improvements

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'This app lets you get a random placeholder from your choice of J. Coles Dreamville label. Enjoy!'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
